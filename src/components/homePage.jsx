bc5acf990997db9108c98e57b6d48b138e15495c
Wojciech Rogoz <wrogoz@gmail.com>
2020-08-26 11:48:56 +0200

    homePage styling to cons 1.0

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,54 +3,47 @@ import styled from "styled-components";
 import Carousel from "react-bootstrap/Carousel";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image"
+import Image from "react-bootstrap/Image";
 import img1 from "../images/startPage/img1.jpg";
 import img2 from "../images/startPage/img2.jpg";
 
 const HomePage = () => {
   return (
-    <Test>
+    <Row>
       <HomeBox>
-        <Slider indicators={false} controls={false} pause={false} >
-          <CarouselItem >
-            <SliderImage
-              className="d-block "
-              src={img1}
-              alt="First slide"
-            />
-          </CarouselItem>
-          <CarouselItem >
-            <SliderImage
-              className="d-block "
-              src={img2}
-              alt="second slide"
-            />
-          </CarouselItem>
+        <Slider indicators={false} controls={false} pause={false}>
+          <Carousel.Item>
+            <SliderImage className="d-block " src={img1} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <SliderImage className="d-block " src={img2} alt="second slide" />
+          </Carousel.Item>
         </Slider>
       </HomeBox>
-    </Test>
+    </Row>
   );
 };
-const Test = styled(Row)``;
+
 const HomeBox = styled(Col)`
   display: flex;
   justify-content: center;
 `;
-const CarouselItem = styled(Carousel.Item)`
-  position: relative;
-  overflow:hidden;
-  width:auto;
-  `
+
 const Slider = styled(Carousel)`
   width: 100%;
+  position: relative;
+  overflow: hidden;
 `;
 const SliderImage = styled(Image)`
-        height:70vh;
-        width:100%;
-       
-        @media(min-width:800px){
-         
-          height:73vh;
-        }
-    `;
+  height: 70vh;
+  width: auto;
+  overflow: hidden;
+
+  @media (min-width: 800px) {
+    height: auto;
+    max-height: 71vh;
+    min-width: 100%;
+    overflow: hidden;
+  }
+`;
 export default HomePage;
