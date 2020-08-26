6ed9bb6a2118c155ae57c782e900f6fe0e58c352
Wojciech Rogóż <wrogoz@gmail.com>
2020-08-26 14:15:36 +0200

    homePage @media maxHeight 640 styling

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -38,7 +38,9 @@ const SliderImage = styled(Image)`
   height: 70vh;
   width: auto;
   overflow: hidden;
-
+  @media(max-height:641px){
+    max-height:65vh;
+  }
   @media (min-width: 800px) {
     height: auto;
     max-height: 71vh;
